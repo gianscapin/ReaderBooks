8fe59d8a3112a1406dcb1d1bb2414bf2999b9c8d
gianfrancoscapin <gianscapin96@gmail.com>
2022-07-11 02:08:45 -0300

    login ui

--- app/src/main/java/com/gscapin/readbookcompose/screens/login/LoginScreen.kt
+++ app/src/main/java/com/gscapin/readbookcompose/screens/login/LoginScreen.kt
@@ -1,15 +1,13 @@
 package com.gscapin.readbookcompose.screens.login
 
+import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -18,10 +16,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.gscapin.readbookcompose.components.EmailInput
+import com.gscapin.readbookcompose.components.PasswordInput
 import com.gscapin.readbookcompose.components.ReaderLogo
 
 @Composable
@@ -32,6 +33,9 @@ fun LoginScreen(navController: NavController) {
             verticalArrangement = Arrangement.Top
         ) {
             ReaderLogo()
+            UserForm(loading = false, isCreateAccount = false) { email, pass ->
+                Log.d("Form", "Login: $email, $pass")
+            }
         }
     }
 }
@@ -39,7 +43,11 @@ fun LoginScreen(navController: NavController) {
 @OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
-fun UserForm() {
+fun UserForm(
+    loading: Boolean = false,
+    isCreateAccount: Boolean = false,
+    onDone: (String, String) -> Unit = { email, pass -> }
+) {
     val email = rememberSaveable {
         mutableStateOf("")
     }
@@ -66,6 +74,52 @@ fun UserForm() {
             rememberScrollState()
         )
     Column(modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        EmailInput(
+            emailState = email,
+            enabled = !loading,
+            onAction = KeyboardActions { passwordFocusRequest.requestFocus() }
+        )
 
+        PasswordInput(
+            modifier = Modifier.focusRequester(passwordFocusRequest),
+            passwordState = password,
+            labelId = "Password",
+            enabled = !loading,
+            passwordVisibility = passwordVisibility,
+            onAction = KeyboardActions {
+                if (!valid) return@KeyboardActions
+                onDone(email.value.trim(), password.value.trim())
+            }
+        )
+
+        SubmitButton(
+            textId = if (isCreateAccount) "CreateAccount" else "Login",
+            loading = loading,
+            validInputs = valid
+        ){
+            onDone(email.value.trim(), password.value.trim())
+        }
+
+    }
+}
+
+@Composable
+fun SubmitButton(
+    textId: String,
+    loading: Boolean,
+    validInputs: Boolean,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth(),
+        enabled = !loading && validInputs,
+        shape = CircleShape
+        
+    ) {
+        if(loading) CircularProgressIndicator(modifier = Modifier.size(25.dp))
+        else Text(text = textId, modifier = Modifier.padding(5.dp))
     }
 }
